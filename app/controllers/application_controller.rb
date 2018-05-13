6f5db8e6539a19a481e843261de343f7d6790040
tidharm <tidhar.meltzer@whitesourcesoftware.com>
2018-05-13 14:14:39 +0300

    Added home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "<h1>hello, world!</h1>"
+    render html: "hello, world!"
   end
 end
